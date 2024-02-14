f7c306f631fd08966a7bd9162eab1a65e6d7e893
Mohit <118717989+Mohitsinghparmarg@users.noreply.github.com>
2024-02-14 22:20:23 +0530

    Add files via upload

---
+++ Episode-02-Ignite Our App/App.js
@@ -0,0 +1,45 @@
+  import React from "react";
+  import  ReactDOM  from "react-dom/client";
+ /**
+  * 
+  *  <div id="parent">
+  *   <div> id="child"
+  *        <h1>I am an h1 tag</h1>
+  *       <h2>I am an h2 tag</h2>
+  *    </div>
+  *  </div>
+  * 
+  * 
+  * ReactElement(Object)=> HTML(Browser Understands)
+  * 
+  */ 
+
+ const parent = React.createElement(
+           "div",{id:"parent"},
+         React.createElement(
+            "div",{id:"child"},[
+         React.createElement("h1",{},"I am an h1 tag"),
+         React.createElement("h2",{},"I am an h2 tag")
+        ]),
+        React.createElement(
+            "div",{id:"child2"},[
+         React.createElement("h1",{},"I am an h1 tag"),
+         React.createElement("h2",{},"I am an h2 tag")
+        ])
+ );
+
+//JSX
+
+
+// const heading = React.createElement(
+//     "h1",
+//    {id:"Heading",className:"xyz"},
+//      "Hello world using React"
+//     );
+
+// console.log(heading); // Object
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
